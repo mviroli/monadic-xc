296b07a121981b6efe4c1e1d2210a55d0c91f3bb
mirko.viroli <mirko.viroli@unibo.it>
2025-04-11 23:29:49 +0200

    function call and branching

--- src/main/scala/scafi/AggregateFramework.scala
+++ src/main/scala/scafi/AggregateFramework.scala
@@ -9,63 +9,82 @@ object AggregateFramework extends App:
     case Rep(res: A, nest: Tree[A])
     case Val(res: A)
     case Next(left: Tree[Any], right: Tree[A])
-    case Call(res: A, nest: Tree[A], function: () => A)
+    case Call(fun: () => Any, nest: Tree[A])
     case Empty()
+
     def top: A = this match
       case Rep(a, _) => a
       case Val(a) => a
       case Next(_, r) => r.top
-      case Call(a, _, _) => a
+      case Call(_, n) => n.top
+
+  object Tree:
+    def functionEquality(f1: Any, f2: Any): Boolean = f2 == f2
 
   trait Aggregate[A]:
     given Set[Device] = Set()
+
     def flatMap[B](f: A => Aggregate[B]): Aggregate[B] = Aggregate:
-        case (Next(t1, t2), domain) =>
-          val t1o = this.eval(t1.asInstanceOf[Tree[A]])(using domain)
-          Next(t1o, f(t1o.top).eval(t2)(using domain))
-        case (Empty(), domain) =>
-          val t1o = this.eval(Empty[A]())(using domain)
-          Next(t1o, f(t1o.top).eval(Empty())(using domain))
+      case (Next(t1, t2), domain) =>
+        val t1o = this.eval(t1.asInstanceOf[Tree[A]])(using domain)
+        Next(t1o, f(t1o.top).eval(t2)(using domain))
+      case (Empty(), domain) =>
+        val t1o = this.eval(Empty[A]())(using domain)
+        Next(t1o, f(t1o.top).eval(Empty())(using domain))
+
     def map[B](f: A => B): Aggregate[B] = flatMap(a => f(a))
+
     def eval(input: Tree[A])(using domain: Set[Device]): Tree[A]
 
   object Aggregate:
     opaque type Device = Int
     val selfDevice: Device = 0
+
     def apply[A](round: (Tree[A], Set[Device]) => Tree[A]): Aggregate[A] = new Aggregate[A]:
       override def eval(input: Tree[A])(using domain: Set[Device]): Tree[A] = round(input, domain)
 
     def compute[A](a: A): Aggregate[A] = Aggregate:
       (_, _) => Val(a)
+
     def rep[A](a: A)(f: A => Aggregate[A]): Aggregate[A] = Aggregate:
       case (Empty(), _) => Rep(a, Empty())
       case (_, domain) if !domain.contains(selfDevice) => Rep(a, Empty())
       case (Rep(x, nest), domain) => val n = f(x).eval(nest)(using domain); Rep(n.top, n)
-    def aggregateCall[A](f: Aggregate[() => A]): Aggregate[A] =
-      for
-        fun <- f
-      yield fun()
-    def branch[A](cond: Aggregate[Boolean])(th: Aggregate[A])(el: Aggregate[A]): Aggregate[A] =
-      aggregateCall:
-        for
-          b <- cond
-          f1 <- th.map(() => _)
-          f2 <- el.map(() => _)
-        yield if b then f1 else f2
+
+    def aggregateCall[A](f: Aggregate[() => Aggregate[A]]): Aggregate[A] =
+      val fun = f.eval(Empty())(using Set()).top
+      Aggregate:
+        case (Empty(), domain) => Call(fun, fun().eval(Empty())(using domain))
+        case (Call(g, nest), domain) if g == fun => Call(fun, fun().eval(nest)(using domain))
+        case (Call(_, nest), domain) => Call(fun, fun().eval(Empty())(using domain - selfDevice))
 
     given [A]: Conversion[A, Aggregate[A]] = a => compute(a)
 
   object AdditionalConstructs:
     def nbr[A](a: Aggregate[A]): Aggregate[A] = ???
+
     def hood[A](initial: A, op: (A, A) => A)(a: Aggregate[A]): Aggregate[A] = ???
-    def branch[A](b: Aggregate[Boolean])(th: Aggregate[A])(el: Aggregate[A]): Aggregate[A] = ???
+
     def mux[A](b: Aggregate[Boolean])(th: Aggregate[A])(el: Aggregate[A]): Aggregate[A] = ???
+
     def nbr_range: Aggregate[Double] = ???
+
     def bind[A, B, C](s1: Aggregate[A], s2: Aggregate[B])(f: (A, B) => C): Aggregate[C] = ???
+
     def bind[A, B, C, D](s1: Aggregate[A], s2: Aggregate[B], s3: Aggregate[C])(f: (A, B, C) => D): Aggregate[D] = ???
+
     extension [A](a: Aggregate[A]) def >>[B](f: A => Aggregate[B]): Aggregate[B] = ???
 
+    def branch[A](cond: Aggregate[Boolean])(th: Aggregate[A])(el: Aggregate[A]): Aggregate[A] =
+      aggregateCall:
+        for
+          b <- cond
+          f1 <- compute(() => th)
+          f2 <- compute(() => el)
+        yield if b then f1 else f2
+
   object Showcase:
+
     import AdditionalConstructs.{*, given}
 
     def gradient(src: Boolean): Aggregate[Double] =
@@ -91,6 +110,7 @@ object AggregateFramework extends App:
       rep(from)(n => for c <- rep(1)(identity) yield c + n)
 
   object FancyLib:
+
     import AdditionalConstructs.*
 
     def counterWithNesting2(from: Int): Aggregate[Int] =
--- src/test/scala/scafi/RepTest.scala
+++ src/test/scala/scafi/RepTest.scala
@@ -2,9 +2,11 @@ package scafi
 
 import org.scalatest.matchers.should.Matchers.*
 import AggregateFramework.*
+import org.scalatest.Ignore
 
 class RepTest extends org.scalatest.funsuite.AnyFunSuite:
   given dom: Set[Device] = Set(selfDevice)
+  import AggregateFramework.Aggregate.given
 
   test("Constant rep"):
     val k = constant(5)
@@ -24,3 +26,35 @@ class RepTest extends org.scalatest.funsuite.AnyFunSuite:
     c.eval(Empty()) shouldBe Rep(0, Empty())
     c.eval(Rep(0, Empty())) shouldBe Rep(1, Next(Rep(1, Empty()), Val(1)))
     c.eval(Rep(1, Next(Rep(1, Empty()), Val(1)))) shouldBe Rep(2, Next(Rep(1, Val(1)), Val(2)))
+
+  test("Call to a non-aggregate program"):
+    val f = () => compute(1)
+    val c = aggregateCall(f)
+    c.eval(Empty()) shouldBe Call(f, Val(1))
+
+  test("Call to an aggregate program"):
+    val f = () => counter(0)
+    val c = aggregateCall(f)
+    c.eval(Empty()) shouldBe Call(f, Rep(0, Empty()))
+    c.eval(Call(f, Rep(0, Empty()))) shouldBe Call(f, Rep(1, Val(1)))
+
+  test("Rep restarts in case of a synthetic function change"):
+    val f1 = () => counter(0)
+    val f2 = () => counter(0)
+    val c1 = aggregateCall(f1)
+    val c2 = aggregateCall(f2)
+    c1.eval(Call(f1, Rep(0, Empty()))) shouldBe Call(f1, Rep(1, Val(1)))
+    c2.eval(Call(f1, Rep(0, Empty()))) shouldBe Call(f2, Rep(0, Empty()))
+
+  test("Branching two reps"):
+    import AdditionalConstructs.*
+    val b = branch(true)(counter(0))(counter(100))
+    val b2 = branch(false)(counter(0))(counter(100))
+    val Call(f, n1) = b.eval(Empty()); n1 shouldBe Rep(0, Empty())
+    val Call(`f`, n2) = b.eval(Call(f, n1)); n2 shouldBe Rep(1, Val(1))
+    // switching branch
+    val Call(g, n3) = b2.eval(Call(f, n2)); n3 shouldBe Rep(100, Empty())
+    val Call(`g`, n4) = b2.eval(Call(g, n3)); n4 shouldBe Rep(101, Val(101))
+    // switching branch, rep restarted
+    val Call(`f`, n5) = b.eval(Call(g, n4)); n5 shouldBe Rep(0, Empty())
+
