a6adb7bfc19ab8012f6ed39cfd3fec9c4912f2e3
mirko <mirko.viroli@unibo.it>
2026-02-22 18:00:34 +0100

    sensors and hopGradient

--- src/main/scala/monadicXc/Executor.scala
+++ src/main/scala/monadicXc/Executor.scala
@@ -19,12 +19,41 @@ trait Executor:
       ct = ct + 1
       a.run(selfDevice)(if summon[DomainReset].contains(ct) then Map() else Map(selfDevice -> cn._3))
 
-  class DistributedSystem[A](var aggregate: Aggregate[A], topology: Map[Device, Set[Device]]):
-    var envs: Map[Device, Context] = topology.view.mapValues(_ => Map()).toMap//topology.keySet.map(d => d -> Map(d -> emptyMessage())).toMap.asInstanceOf[Map[Device, Context]]
+  case class Displacement[P](positions: Map[Device, P], topology: Map[Device, Set[Device]]):
+    def apply(p: P): Device = positions.find((k, v) => v == p).get._1
+    def apply(d: Device): P = positions(d)
+
+  object Displacement:
+    def grid(n: Int, m: Int): Displacement[(Int, Int)] =
+      val positions = (for x <- 0 until n; y <- 0 until m yield newDevice() -> (x, y)).toMap
+      val topology = (for
+        d1 -> (x1, y1) <- positions.toSet
+        d2 -> (x2, y2) <- positions.toSet
+        if (x1, y1) == (x2, y2) || Math.abs(x2 - x1) + Math.abs(y2 - y1) == 1
+      yield d1 -> d2).groupMap(_._1)(_._2)
+      Displacement(positions, topology)
+
+  case class Platform(topology: Map[Device, Set[Device]] = Map(), ssns: Map[String, Map[Device, Any]] = Map()):
+    def withSensor[B](name: String, values: Map[Device, B], default: B = null): Platform =
+      val toAdd = if default == null then Map() else Map.from(topology.keys.map(_ -> NValue(default)))
+      var newValues = (toAdd ++ values.map((k, v) => (k, v))).asInstanceOf[Map[Device, Any]]
+      this.copy(ssns = ssns + (name -> newValues))
+
+    def withNeighbourhood(mapping: (Device, Set[Device])): Platform = this.copy(topology = topology + mapping)
+
+    def withTopology(topology: Map[Device, Set[Device]]): Platform = this.copy(topology = topology)
+
+    def asDistributedSystem[A](aggr: DistributedSystem[A] ?=> Aggregate[A]): DistributedSystem[A] =
+      val ds = new DistributedSystem[A](this)
+      ds.aggregate = aggr(using ds)
+      ds
+
+
+  class DistributedSystem[A](val platform: Platform):
+    var aggregate: Aggregate[A] = _
+    var envs: Map[Device, Context] = platform.topology.view.mapValues(_ => Map()).toMap//topology.keySet.map(d => d -> Map(d -> emptyMessage())).toMap.asInstanceOf[Map[Device, Context]]
     private var _snss: Map[String, Map[Device, Any]] = Map()
     private var _currentDevice = selfDevice
-
-    def this(topology: Map[Device, Set[Device]]) = this(null, topology)
 
     def withAggregate(aggr: DistributedSystem[A] ?=> Aggregate[A]): this.type = try this finally this.aggregate = aggr(using this)
 
@@ -33,12 +62,13 @@ trait Executor:
     def fire(d: Device): A =
       _currentDevice = d
       val (ctx, a, msg) = aggregate.run(_currentDevice)(envs(d))
-      envs = topology(d).foldLeft(envs)((e, dd) => e + (dd -> (envs(dd) + (d -> msg))))
+      envs = platform.topology(d).foldLeft(envs)((e, dd) => e + (dd -> (envs(dd) + (d -> msg))))
       a
 
-    /*
-    def fires(ds: Device*): Seq[Export[A]] = ds.map(fire(_))
-
+    def fires(ds: Device*): Seq[A] = ds.map(fire(_))
   object DistributedSystem:
-    def platformSensor[A, B](name: String): DistributedSystem[B] ?=> () => A = () => summon[DistributedSystem[B]]._snss(name)(summon[DistributedSystem[B]]._currentDevice).asInstanceOf[A]
-    */
+    def platformSensor[A, B](name: String): DistributedSystem[B] ?=> Aggregate[A] =
+      sensor:
+        () => summon[DistributedSystem[B]].platform.ssns(name)(summon[DistributedSystem[B]]._currentDevice).asInstanceOf[A]
+
+  export DistributedSystem.platformSensor
--- src/test/scala/monadicXc/BasicAggregateTest.scala
+++ src/test/scala/monadicXc/BasicAggregateTest.scala
@@ -93,7 +93,11 @@ class BasicAggregateTest extends org.scalatest.funsuite.AnyFunSuite:
   test("Ping-pong"):
     val ag = retsend(0)(nv => nv.nvMap(_ + 1))
     val (d1, d2, d3) = (newDevice(), newDevice(), newDevice())
-    val ds = DistributedSystem[NValue[Int]](ag, Map(d1 -> Set(d1, d2, d3), d2 -> Set(d1, d2, d3), d3 -> Set(d1, d2, d3)))
+    val ds = Platform()
+      .withNeighbourhood(d1 -> Set(d1, d2, d3))
+      .withNeighbourhood(d2 -> Set(d1, d2, d3))
+      .withNeighbourhood(d3 -> Set(d1, d2, d3))
+      .asDistributedSystem(ag)
     ds.fire(d1) shouldBe toNValue(1)
     ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2))
     ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2, d2 -> 2))
@@ -104,123 +108,78 @@ class BasicAggregateTest extends org.scalatest.funsuite.AnyFunSuite:
     ds.fire(d2) shouldBe NValue(1, Map(d1 -> 4, d2 -> 6))
     ds.fire(d1) shouldBe NValue(1, Map(d1 -> 3, d2 -> 5))
 
-/*
-test("Elaborating on a rep")
 
-import scafi.lib.AggregateLib.counter
+  test("Branching ping-pong with a sensor"):
+    var cond = false
+    val agf:Aggregate[NValue[Int]] = sensor(() => cond).flatMap(b => branch(b)(toNValue(0))(retsend(0)(nv => nv.nvMap(_ + 1))))
+    val (d1, d2) = (newDevice(), newDevice())
+    val ds = Platform()
+      .withNeighbourhood(d1 -> Set(d1, d2))
+      .withNeighbourhood(d2 -> Set(d1, d2))
+      .asDistributedSystem(agf)
+    ds.fire(d1) shouldBe NValue(1)
+    ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2))
+    ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2, d2 -> 2))
+    cond = true // sensor change
+    ds.fire(d2) shouldBe NValue(0)
+    cond = false // sensor change
+    ds.fire(d1) shouldBe NValue(1, Map(d1 -> 2))
+    ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2))
+    ds.fire(d2) shouldBe NValue(1, Map(d1 -> 2, d2 -> 2))
 
-val ag = counter(0).aMapN(_ < 3)
-ag.repeat().take(4).map(_.top.asValue) shouldBe List(true, true, false, false)
+  test("Folding a ping-pong"):
+    val ag = retsend(0)(_.nvMap(_ + 1)).flatMap(nv => fold(0)(_ max _)(nv))
+    val (d1, d2, d3) = (newDevice(), newDevice(), newDevice())
+    val ds = Platform()
+      .withNeighbourhood(d1 -> Set(d1, d2, d3))
+      .withNeighbourhood(d2 -> Set(d1, d2, d3))
+      .withNeighbourhood(d3 -> Set(d1, d2, d3))
+      .asDistributedSystem(ag)
+    ds.fires(d1, d2, d2, d1, d2) shouldBe List(0, 2, 2, 3, 4)
 
 
-test("Call to a non-aggregate program"):
-  val f: () => Aggregate[Int] = () => 5
-  val ag = call(f)
-  ag.repeat().take(4).map(_.top.asValue) shouldBe List(5, 5, 5, 5)
-
-
-test("Call to an aggregate program"):
-  val ag = call(() => counter(0))
-  ag.repeat().take(4).map(_.top.asValue) shouldBe List(1, 2, 3, 4)
-
-test("Muxing by a rep"):
-  val agb: Aggregate[Boolean] = counter(0).aMapN(_ < 3)
-  val ag = mux(agb)(1)(2)
-  ag.repeat().take(4).map(_.top.asValue) shouldBe List(1, 1, 2, 2)
-
-test("Muxing with restart"):
-  val agb = counter(0).aMapN(vc => vc < 3 || vc > 4)
-  val ag = mux(agb)(counter(0))(100)
-  ag.repeat().take(6).map(_.top.asValue) shouldBe List(1, 2, 100, 100, 5, 6)
-
-test("Branching with restart"):
-  val agb = counter(0).aMapN(vc => vc < 3 || vc > 4)
-  val ag = branch(agb)(counter(0))(100)
-  ag.repeat().take(6).map(_.top.asValue) shouldBe List(1, 2, 100, 100, 1, 2)
-
-test("Ping-pong"):
-  val ag = retsend(0)(_.aMapN(_ + 1))
-  val (d1, d2, d3) = (newDevice(), newDevice(), newDevice())
-  val ds = Platform()
+  val List(d1, d2, d3, d4) = List.fill(4)(newDevice())
+  val platform3 = Platform()
     .withNeighbourhood(d1 -> Set(d1, d2, d3))
     .withNeighbourhood(d2 -> Set(d1, d2, d3))
     .withNeighbourhood(d3 -> Set(d1, d2, d3))
-    .asDistributedSystem(ag)
-  ds.fire(d1).top.asValue shouldBe 1
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 1))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 2))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 3))
-  ds.fire(d1).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 3))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 4, d2 -> 4))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 4, d2 -> 5))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 4, d2 -> 6))
-  ds.fire(d1).top shouldBe MapWithDefault(1, Map(d1 -> 3, d2 -> 5))
-  ds.fire(d3).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 2, d3 -> 1))
-  ds.fire(d3).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 2, d3 -> 2))
-  ds.fire(d1).top shouldBe MapWithDefault(1, Map(d1 -> 4, d2 -> 5, d3 -> 3))
-
-
-test("Branching ping-pong with a sensor"):
-  var cond = false
-  val agf = branch(sensor(cond))(0)(retsend(0)(_.aMapN(_ + 1)))
-  val (d1, d2) = (newDevice(), newDevice())
-  val ds = Platform()
-    .withNeighbourhood(d1 -> Set(d1, d2))
-    .withNeighbourhood(d2 -> Set(d1, d2))
-    .asDistributedSystem(agf)
-  ds.fire(d1).top.asValue shouldBe 1
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 2))
-  cond = true // sensor change
-  ds.fire(d2).top.asValue shouldBe 0
-  cond = false // sensor change
-  ds.fire(d1).top shouldBe MapWithDefault(1, Map(d1 -> 2))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2))
-  ds.fire(d2).top shouldBe MapWithDefault(1, Map(d1 -> 2, d2 -> 2))
-
-test("Folding a ping-pong"):
-  val ag = fold(0)(_ max _)(retsend(0)(_.aMapN(_+1)))
-
-  val (d1, d2, d3) = (newDevice(), newDevice(), newDevice())
-  val ds = Platform()
-    .withNeighbourhood(d1 -> Set(d1, d2, d3))
-    .withNeighbourhood(d2 -> Set(d1, d2, d3))
-    .withNeighbourhood(d3 -> Set(d1, d2, d3))
-    .asDistributedSystem(ag)
-  ds.fires(d1, d2, d2, d1, d2).map(_.top.asValue) shouldBe List(0, 2, 2, 3, 4)
-
-val List(d1, d2, d3, d4) = List.fill(4)(newDevice())
-val platform3 = Platform()
-  .withNeighbourhood(d1 -> Set(d1, d2, d3))
-  .withNeighbourhood(d2 -> Set(d1, d2, d3))
-  .withNeighbourhood(d3 -> Set(d1, d2, d3))
-val platformAdHoc = Platform()
-  .withNeighbourhood(d1 -> Set(d1, d2))
-  .withNeighbourhood(d2 -> Set(d1, d2, d3))
-  .withNeighbourhood(d3 -> Set(d2, d3, d4))
-  .withNeighbourhood(d4 -> Set(d2, d3, d4))
-
-test("hopGradient"):
-  val ds = Platform()
+  val platformAdHoc = Platform()
     .withNeighbourhood(d1 -> Set(d1, d2))
     .withNeighbourhood(d2 -> Set(d1, d2, d3))
     .withNeighbourhood(d3 -> Set(d2, d3, d4))
     .withNeighbourhood(d4 -> Set(d2, d3, d4))
-    .withSensor("src", Map(d1 -> true, d2 -> false, d3 -> false, d4 -> false))
-    .asDistributedSystem:
-      def hopGradient(src: Aggregate[Boolean]): Aggregate[Int] =
-        retsend(Int.MaxValue): v =>
-          mux(src)(0):
-            fold(Int.MaxValue)(_ min _):
-              v.aMapN(n => if n == Int.MaxValue then n else n + 1)
-      hopGradient(platformSensor("src"))
 
-  Seq(
-    d2 -> Int.MaxValue, d1 -> 0, d2 -> 1, d4 -> Int.MaxValue, d3 -> 2, d4 -> 3
-  ).foreach: (device, result) =>
-    ds.fire(device).top.asValue shouldBe result
+  test("platform sensor"):
+    val ds = Platform()
+      .withNeighbourhood(d1 -> Set(d1))
+      .withNeighbourhood(d2 -> Set(d2))
+      .withNeighbourhood(d3 -> Set(d3))
+      .withNeighbourhood(d4 -> Set(d4))
+      .withSensor("src", Map(d1 -> true, d2 -> false, d3 -> false, d4 -> false))
+      .asDistributedSystem:
+        platformSensor("src")
 
+    Seq(
+      d2 -> false, d1 -> true, d3 -> false, d4 -> false
+    ).foreach: (device, result) =>
+      ds.fire(device) shouldBe result
 
+  test("hopGradient"):
+    val ds = Platform()
+      .withNeighbourhood(d1 -> Set(d1, d2))
+      .withNeighbourhood(d2 -> Set(d1, d2, d3))
+      .withNeighbourhood(d3 -> Set(d2, d3, d4))
+      .withNeighbourhood(d4 -> Set(d2, d3, d4))
+      .withSensor("src", Map(d1 -> true, d2 -> false, d3 -> false, d4 -> false))
+      .asDistributedSystem:
+        hopGradient(platformSensor("src"))
+
+    Seq(
+      d2 -> Int.MaxValue, d1 -> 0, d2 -> 1, d4 -> Int.MaxValue, d3 -> 2, d4 -> 3
+    ).foreach: (device, result) =>
+      ds.fire(device) shouldBe result
+
+/*
 test("mid"):
   val ds = platform3
     .withSensor("mid", Map(d1 -> 1, d2 -> 2, d3 -> 3))
